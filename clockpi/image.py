2bc8186a7e8662f52a0bf4d4a00d48d10b3b9423
Alex Lim <kaitwee@gmail.com>
2025-03-29 23:32:44 +0800

    updat

--- clockpi/image.py
+++ clockpi/image.py
@@ -1,33 +1,20 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-
-from itertools import product
-import sys
 import os
 import logging
-import time
-import traceback
-import datetime
-
-DIR_IMG:str = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'img')
-DIR_LIB:str = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(DIR_LIB):
-    sys.path.append(DIR_LIB)
-
-from PIL import Image, ImageDraw, ImageFont, ImagePalette
 import numpy as np
-#from waveshare_epd import epd7in3e
+from PIL.Image import Image
 
-logging.basicConfig(level=logging.DEBUG)
+# Constants
+CONST_IMG_WIDTH:int = 800 # pixel count of the Waveshare E-paper display 7"3e
+CONST_IMG_HEIGHT:int = 480 # pixel count of the Waveshare E-paper display 7"3e
+CONST_IMG_NC:int = 2 # colors per channel
 
-
-def crop(img:Image) -> Image:
-    left:float = (img.width - 800) * 0.5
-    right:float = left + 800
-    top:float = (img.height - 480) * 0.5
-    bottom:float = top + 480
+def crop(img:Image, w:int, h:int) -> Image:
+    l:float = (img.width - w) * 0.5
+    r:float = l + w
+    t:float = (img.height - h) * 0.5
+    b:float = t + h
     
-    return img.crop((left, top, right, bottom))
+    return img.crop((l, t, r, b))
 
 
 def get_new_val(old_val, nc):
@@ -35,8 +22,8 @@ def get_new_val(old_val, nc):
 
 
 # Floyd-Steinberg dither the image img into a palette with nc colours per channel.
+# https://scipython.com/blog/floyd-steinberg-dithering/
 def fs_dither(img:Image, nc:int) -> Image:
-
     h:int = img.height
     w:int = img.width
     arr = np.array(img, dtype=float) / 255
@@ -62,8 +49,8 @@ def fs_dither(img:Image, nc:int) -> Image:
     return Image.fromarray(carr)
 
 
+# Simple palette reduction without dithering.
 def palette_reduce(img:Image, nc:int) -> Image:
-    """Simple palette reduction without dithering."""
     arr = np.array(img, dtype=float) / 255
     arr = get_new_val(arr, nc)
 
@@ -71,30 +58,30 @@ def palette_reduce(img:Image, nc:int) -> Image:
     return Image.fromarray(carr)
 
 
-def main(file_path:str) -> None:
+def procsess_image(src_path:str, dest_path:str, del_src:bool = True) -> None:
     try:
-        img = Image.open(file_path)
-        nc:int = 2 # number or color components
+        img:Image = Image.open(src_path)
+        w:int = img.width
+        h:int = img.height
         
         # resize
-        ratio:float = max(800/img.width, 480/img.height);
-        img.thumbnail((img.width*ratio, img.height*ratio), Image.Resampling.LANCZOS)
+        r:float = max(CONST_IMG_WIDTH/w, CONST_IMG_HEIGHT/h);
+        img.thumbnail((w * r, h * r), Image.Resampling.LANCZOS)
         
         # Crop
         img:Image = crop(img) 
         
-        img = fs_dither(img, 2)
-        img = palette_reduce(img, 2)
-        #img.show()
+        # Apply fyold steinburg dithering
+        img = fs_dither(img, CONST_IMG_NC)
         
-        path_list:list[str] = os.path.splitext(file_path)
-        out_path:str = path_list[0] + "_small" + path_list[-1]
-        img.save(out_path, "png")
+        # Reduce palette color
+        img = palette_reduce(img, CONST_IMG_NC)
+        
+        # Save file
+        img.save(dest_path)
+        
+        if del_src:
+            os.remove(src_path)
         
     except IOError as error:
         logging.error(error)
-        
-
-if __name__ == "__main__":
-    file_path:str = sys.argv[1]
-    main(file_path)
