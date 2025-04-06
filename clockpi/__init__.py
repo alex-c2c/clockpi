e7280545b749a087af11294079d6a0b3f7028b90
Alex Lim <kaitwee@gmail.com>
2025-04-06 15:54:23 +0800

    update

--- clockpi/__init__.py
+++ clockpi/__init__.py
@@ -20,7 +20,7 @@ def celery_init_app(app: Flask) -> Celery:
 
 
 def redis_init_app(app: Flask) -> FlaskRedis:
-    redis_client = FlaskRedis(app)
+    redis_client = FlaskRedis(app, decode_responses=True)
     app.extensions["redis"] = redis_client
     return redis_client
 
